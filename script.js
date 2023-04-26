55ffac7e94f942a99b3fd18afed345d2e20c9687
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-04-26 16:25:51 +0530

    calling backend api

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-let page = document.querySelector('#page');
+let page = document.querySelector('#page').textContent;
+console.log(page);
 
-
-if(page==="Signup"){
+if(page==="Signup")  { 
 
 let Username = document.querySelector('#name');
 let Email = document.querySelector('#email');
 let Phone = document.querySelector('#phone');
 let Password = document.querySelector('#password');
 let ConfirmPw = document.querySelector('#confirmPw');
+let forms = document.querySelector('form');
+let mismatch = document.querySelector('#mismatch');
+let exists  = document.querySelector('#exist');
+let btn = document.querySelector('#signupbtn')
 
-if(Password != ConfirmPw ) {
-     
+btn.addEventListener('click',()=>{
+    mismatch.style.display='none';
+    exists.style.display='none';
+})
 
-   } 
+forms.onsubmit = signup;
 
+async function signup (event) {
+    event.preventDefault();
 
+    let obj = {
+        Name : Username.value,
+        Email : Email.value,
+        Phone : Phone.value,
+        Password : Password.value
+    }
+    console.log(obj);
+    
+    if( Password.value === ConfirmPw.value ) {
+        let res = await postUser(obj);
+        console.log(res)
+        if(res.response.data.message==='user exists'){
+            exists.style.display="block";
+        }
+    } else{
+         mismatch.style.display = "block"
+       } 
+}
+
+async function postUser(myobj){
+    try{
+    let response = await axios.post('http://localhost:8000/user/signup',myobj);
+    console.log(response);
+    return response
+    }
+    catch(err){
+    return err;
+    }
+}
 }
