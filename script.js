07d79b2d6341a45fed7a3558a1f99f7e47e1fb92
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-04-26 12:23:28 +0530

    signup page

---
+++ script.js
@@ -0,0 +1,18 @@
+let page = document.querySelector('#page');
+
+
+if(page==="Signup"){
+
+let Username = document.querySelector('#name');
+let Email = document.querySelector('#email');
+let Phone = document.querySelector('#phone');
+let Password = document.querySelector('#password');
+let ConfirmPw = document.querySelector('#confirmPw');
+
+if(Password != ConfirmPw ) {
+     
+
+   } 
+
+
+}
